b610e3542c2c13ec4e6e33c83cf59f916d7c0a09
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-08-23 16:43:40 -0500

    basics for a reservation card

--- front-end/src/reservations/reservation/Reservation.js
+++ front-end/src/reservations/reservation/Reservation.js
@@ -1,5 +1,23 @@
 import React from "react";
 
-function Reservation() {}
+function Reservation({
+  reservation_id,
+  first_name,
+  last_name,
+  mobile_number,
+  reservation_time,
+  people,
+}) {
+  return (
+    <>
+      <h4>
+        Reservation {reservation_id} at {reservation_time} (party of {people})
+      </h4>
+      <p>
+        Contact Details for {first_name} {last_name}: {mobile_number}
+      </p>
+    </>
+  );
+}
 
 export default Reservation;

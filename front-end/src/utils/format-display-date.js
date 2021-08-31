0a23ea3e7bb11d6ce00258361d59e5a4a117ca81
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-08-31 16:17:37 -0500

    fixed Date syntax bug

--- front-end/src/utils/format-display-date.js
+++ front-end/src/utils/format-display-date.js
@@ -1,12 +1,7 @@
 export default function formatDisplayDate(date, format) {
-  const dateObject = new Date(date);
+  const dateObject = new Date(`${date}+"T00:00:00"`);
 
   // takes the "YYYY-MM-DD" format, like "2021-08-10" and returns:
-
-  // "Tue Aug 10 2021"
-  if (!format) {
-    return dateObject.toDateString();
-  }
 
   // "Aug 10 (Tue)"
   if (format === "short") {
@@ -20,14 +15,16 @@ export default function formatDisplayDate(date, format) {
 
     return `${gotMonth} ${gotDate} (${gotDay}) `;
   }
-
   // "Tuesday, Aug 10, 2021"
-  if (format === "long") {
+  else if (format === "long") {
     return dateObject.toLocaleDateString("en-us", {
       weekday: "long",
       year: "numeric",
       month: "short",
       day: "numeric",
     });
+  } else {
+    // "Tue Aug 10 2021"
+    return dateObject.toDateString();
   }
 }

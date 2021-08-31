a295b9fbdeecebcf4f38783a5a5933e0fb804573
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-08-31 10:41:27 -0500

    GET /reservations endpoint responds with all data

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,16 @@
+const service = require("./reservations.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 /**
  * List handler for reservation resources
  */
 async function list(req, res) {
-  res.json({
-    data: [],
-  });
+  const reservations = await service.list();
+  if (reservations.length) {
+    res.locals.data = reservations;
+  }
+  const { data } = res.locals;
+  res.json({ data: data });
 }
 
 module.exports = {

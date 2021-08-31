244fd29ca064ce9580bf842665c4954eeffc6ac9
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-08-31 13:01:46 -0500

    GET /reservations uses date parameter to filter data

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -5,7 +5,8 @@ const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
  * List handler for reservation resources
  */
 async function list(req, res) {
-  const reservations = await service.list();
+  const { date } = req.query;
+  const reservations = await service.list(date);
   if (reservations.length) {
     res.locals.data = reservations;
   }

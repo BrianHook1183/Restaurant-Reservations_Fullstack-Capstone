6c83007df6b386654fa415405a6a862f109f106e
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-09-04 13:49:20 -0500

    POST /reservations validation for all fields

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,9 +1,40 @@
 const service = require("./reservations.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const hasProperties = require("../errors/hasProperties");
 
-/**
- * List handler for reservation resources
- */
+//* Validation vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+
+const VALID_PROPERTIES = [
+  "first_name",
+  "last_name",
+  "mobile_number",
+  "reservation_date",
+  "reservation_time",
+  "people",
+];
+
+function hasOnlyValidProperties(req, res, next) {
+  const { data = {} } = req.body;
+
+  const invalidFields = Object.keys(data).filter(
+    (field) => !VALID_PROPERTIES.includes(field)
+  );
+
+  if (invalidFields.length) {
+    return next({
+      status: 400,
+      message: `Invalid field(s): ${invalidFields.join(", ")}`,
+    });
+  }
+  next();
+}
+
+const hasRequiredProperties = hasProperties(...VALID_PROPERTIES);
+
+//! Validation ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//* CRUD vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+
+// List handler for reservation resources
 async function list(req, res) {
   const { date } = req.query;
   const reservations = await service.list(date);
@@ -12,14 +43,19 @@ async function list(req, res) {
   res.json({ data: data });
 }
 
-// Create a new reservation
+// Create handler for a new reservation
 async function create(req, res) {
-  const newReservation = req.body.data;
-  const data = await service.create(newReservation);
+  const data = await service.create(req.body.data);
   res.status(201).json({ data });
 }
 
+//! CRUD ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
 module.exports = {
-  create: asyncErrorBoundary(create),
+  create: [
+    hasOnlyValidProperties,
+    hasRequiredProperties,
+    asyncErrorBoundary(create),
+  ],
   list: asyncErrorBoundary(list),
 };

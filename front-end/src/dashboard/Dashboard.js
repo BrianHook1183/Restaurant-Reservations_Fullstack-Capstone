e290db98c76a97a61775676f351ed8c70c7a5090
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-09-14 03:34:54 -0500

    expand loadDashboard() to retrieve tables

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import useQuery from "../utils/useQuery";
-import { listReservations } from "../utils/api";
+import { listReservations, listTables } from "../utils/api";
 import formatDisplayDate from "../utils/format-display-date";
 import ErrorAlert from "../layout/ErrorAlert";
 import DateNavigation from "./DateNavigation";
 import ReservationsList from "../reservations/list/ReservationsList";
+import TablesList from "../tables/list/TablesList";
 
 /**
  * Defines the dashboard page.
@@ -24,14 +25,31 @@ function Dashboard({ date }) {
   const [reservations, setReservations] = useState([]);
   const [reservationsError, setReservationsError] = useState(null);
 
-  useEffect(loadDashboard, [date]);
+  const [tables, setTables] = useState([]);
+  const [tablesError, setTablesError] = useState(null);
+
+  //TODO check if using [tables] here is best practice. it is currently clearing a "useEffect has a missing dependency" console warning
+  useEffect(loadDashboard, [date, tables]);
 
   function loadDashboard() {
     const abortController = new AbortController();
     setReservationsError(null);
+    setTablesError(null);
+
+    // listReservations will run every time {date} changes
     listReservations({ date }, abortController.signal)
       .then(setReservations)
+      .then(console.log("listReservations() ran from loadDashboard()"))
       .catch(setReservationsError);
+
+    // listTables only runs on initial loadDashboard() or if tables is empty.
+    if (!tables.length) {
+      listTables(abortController.signal)
+        .then(setTables)
+        .then(console.log("listTables() ran from loadDashboard()"))
+        .catch(setTablesError);
+    }
+
     return () => abortController.abort();
   }
 
@@ -42,13 +60,17 @@ function Dashboard({ date }) {
 
   return (
     <main>
-      <h1>Dashboard</h1>
-      <div className="d-md-flex mb-3">
-        <h4 className="mb-0">Reservations for: {displayDateLong}</h4>
-        <DateNavigation date={date} />
-      </div>
+      {/* <h1>Dashboard</h1> */}
+      {/* <div className="d-md-flex mb-3"> */}
+      <h4>{displayDateLong}</h4>
+      <DateNavigation date={date} />
+      {/* </div> */}
+      <h2 className="mb-0">Reservations:</h2>
       <ReservationsList reservations={reservations} />
       <ErrorAlert error={reservationsError} />
+      <h2 className="mb-0">Tables:</h2>
+      <TablesList tables={tables} />
+      <ErrorAlert error={tablesError} />
     </main>
   );
 }

329c4d5bb39fcb34d8bdecc9c4de2334ed938ab2
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-09-16 05:19:55 -0500

    removed extra code from listTables()

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -88,11 +88,8 @@ export async function postReservation(reservationDetails, signal) {
  * Retrieves all tables
  */
 
-export async function listTables(params, signal) {
+export async function listTables(signal) {
   const url = new URL(`${API_BASE_URL}/tables`);
-  Object.entries(params).forEach(([key, value]) =>
-    url.searchParams.append(key, value.toString())
-  );
   return await fetchJson(url, { headers, signal }, []);
 }
 

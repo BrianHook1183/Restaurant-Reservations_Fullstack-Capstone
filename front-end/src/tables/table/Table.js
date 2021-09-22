f1bc0025bde425e830be906c1c4c9c7ed8ea67ea
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-09-22 02:30:50 -0500

    added windw.confirm in place of react-modal

--- front-end/src/tables/table/Table.js
+++ front-end/src/tables/table/Table.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { finishTable } from "../../utils/api";
+import ErrorAlert from "../../layout/ErrorAlert";
 
 function Table({ table }) {
   const { table_name, table_id, capacity } = table;
   const occupied = table.reservation_id;
+
+  const history = useHistory();
+  const [finishTableError, setFinishTableError] = useState(null);
+
+  const confirmFinish = () => {
+    if (
+      window.confirm(
+        "Is this table ready to seat new guests? This cannot be undone."
+      )
+    ) {
+      const abortController = new AbortController();
+      setFinishTableError(null);
+
+      finishTable(table_id, abortController.signal)
+        // history.go(0) refreshes the current page (should be /dashboard) so that tables effect hook reloads
+        .then(() => history.go(0))
+        .catch(setFinishTableError);
+      return () => abortController.abort();
+    }
+  };
 
   let finish = null;
   if (occupied) {
@@ -10,7 +33,7 @@ function Table({ table }) {
       <button
         className="btn btn-primary"
         data-table-id-finish={`${table_id}`}
-        onClick={}
+        onClick={confirmFinish}
       >
         Finish
       </button>
@@ -26,6 +49,7 @@ function Table({ table }) {
           <div className={`bg-${occupied ? "light" : "success"}`}>
             <h6 className="text-center" data-table-id-status={`${table_id}`}>
               {occupied ? "occupied" : "free"}
+              <ErrorAlert error={finishTableError} />
             </h6>
           </div>
         </li>

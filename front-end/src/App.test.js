99c05aa2286ef143c7b36b031f9947550329093b
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-10-11 06:51:56 -0500

    changing test to look for new app title

--- front-end/src/App.test.js
+++ front-end/src/App.test.js
@@ -9,6 +9,6 @@ test("renders title", () => {
       <App />
     </Router>
   );
-  const restaurant = screen.getByText(/reservation/i);
+  const restaurant = screen.getByText(/Restaurant Reservation System v1.8/i);
   expect(restaurant).toBeInTheDocument();
 });

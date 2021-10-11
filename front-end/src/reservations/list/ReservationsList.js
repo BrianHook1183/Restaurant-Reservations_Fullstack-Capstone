3d446c7086da8082750986e9a5732a3ae982e31e
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-10-11 07:11:13 -0500

    edit to error message

--- front-end/src/reservations/list/ReservationsList.js
+++ front-end/src/reservations/list/ReservationsList.js
@@ -5,7 +5,7 @@ import LoadingWheel from "../../widgets/LoadingWheel";
 function ReservationsList({ reservations }) {
   // JSX
   const noReservationsMessage = (
-    <span className="mx-auto">There are no reservations for that query</span>
+    <span className="mx-auto">No reservations found.</span>
   );
 
   // displays while waiting for api response, and if there are 0 results

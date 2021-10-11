53a1574392afc8f54ea92da9990d8f0729247d88
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-10-11 02:53:49 -0500

    removed Periodic Tables references

--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -16,8 +16,8 @@ function Menu() {
           className="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0"
           to="/"
         >
-          <div className="sidebar-brand-text mx-3">
-            <span>Periodic Tables</span>
+          <div className="sidebar-brand-text text-wrap mx-3">
+            <span>Restaurant Reservation System v1.8</span>
           </div>
         </Link>
         <hr className="sidebar-divider my-0" />
